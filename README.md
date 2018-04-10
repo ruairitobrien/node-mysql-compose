34acba0446a951cabec6d3e1341fb2a1aab2e002
Ruairi O Brien <ruairitobrien@gmail.com>
2018-04-10 19:36:52 +0100

    Added restart for db connect failure

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ This project was created using the [express application generator](https://expre
 To use this setup you need to install [docker](https://www.docker.com/) and [docker compose](https://docs.docker.com/compose/).
 
 
+## Start
+
+`docker-compose up`
+
+> Note, the very first time this is run, mysql will take some time to be created and the node app will fail. The node app will restaet until the connection succeeds. This will only happen the first time docker compose creates these containers.
+
+This will start 2 docker containers. One for mysql. One for the node app. You can access the application at http://localhost:3000
+
+The node app is started with pm2 and will automatically restart whenever you change any server files.
+
+If you want to remove the container you can run:
+`docker-compose rm`
+
+
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var mysql = require("mysql");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 const env = process.env;
 let conf = {
@@ -25,29 +25,30 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      con();
-    } else {
-      console.error("connecting error", err);
-      return;
-    }
-  } else console.log("connecting success");
+    console.error("Database connection error", err);
+    // Bit of a hack to retry until mysql becomes available
+    setTimeout(function () {
+      throw err;
+    }, 1000);
+  } else {
+    console.log("Database connection success");
+  }
 });
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,11 +59,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
